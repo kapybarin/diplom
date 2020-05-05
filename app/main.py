82eae8b4552217eb2d7ce8ba6f657d21ee7e0ae9
Dmitry Konyshev <dk@noirdjinn.dev>
2020-05-05 20:26:52 +0300

    test openapi fix

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, Response, status
+from fastapi.openapi.utils import get_openapi
 from dotenv import load_dotenv
 from pony.orm import db_session
 
@@ -9,6 +10,21 @@ from models_api import NewUser
 load_dotenv()
 app = FastAPI()
 db = setup_database()
+
+def custom_openapi():
+    if app.openapi_schema:
+        return app.openapi_schema
+    openapi_schema = get_openapi(
+        title="TakeAndGo",
+        version="0.0.1",
+        description="Smart bookshelf project",
+        routes=app.routes,
+    )
+    openapi_schema["info"]["x-logo"] = {
+        "url": "https://raw.githubusercontent.com/Svintsova/TakeAndGo/master/public/logo192.png"
+    }
+    app.openapi_schema = openapi_schema
+    return app.openapi_schema
 
 
 @app.get("/")
