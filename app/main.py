805f276306eced1e6414447dd366fef6e4cbbf09
Dmitry Konyshev <dk@noirdjinn.dev>
2020-05-06 20:02:23 +0300

    Fixed error

--- app/main.py
+++ app/main.py
@@ -53,15 +53,15 @@ def read_root():
 @app.post("/user/new")
 @db_session
 def new_user(u: NewUser, res: Response):
-    email_valid, err = is_valid_email(u.email)
+    email_valid, txt = is_valid_email(u.email)
     if email_valid is False:
         res.status_code = status.HTTP_400_BAD_REQUEST
-        return {"error": err}
+        return {"error": txt}
     try:
         curr_user = User.get(email=email_valid)
         return {"error": f'User already exists with the same email and id {curr_user.id}'}
     except RowNotFound:
-        new = User(email=u.email, first_name=u.name, last_name=u.surname, password=u.password)
+        new = User(email=txt, first_name=u.name, last_name=u.surname, password=u.password)
         return {"id": new.id}
 
 
