09b21c33bdd93993d775d07229b0eb77b2a49093
Dmitry Konyshev <dk@noirdjinn.dev>
2020-05-06 20:15:34 +0300

    Test email validation

--- app/tools.py
+++ app/tools.py
@@ -6,6 +6,6 @@ from typing import Tuple
 def is_valid_email(email:str) -> Tuple[bool, str]:
     try:
         valid = validate_email(email)
-        return True, valid.email
+        return True, email
     except EmailNotValidError as e:
         return False, str(e)
