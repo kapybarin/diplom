614ef1dbdfa691ced7f9e443bf0b188d726f0240
Dmitry Konyshev <dk@noirdjinn.dev>
2020-05-07 20:48:15 +0300

    Remove pgadmin from networks docker

--- start.sh
+++ start.sh
@@ -2,5 +2,4 @@
 docker-compose down --volumes --rmi all
 docker-compose up --force-recreate -d
 docker network connect bridge full_app
-docker network connect bridge full_pgadmin
 docker network connect bridge full_db_postgres
