debd8d92263a518612a586c79b1e5af492ec2a97
Dmitry Konyshev <dk@noirdjinn.dev>
2020-05-09 16:31:08 +0300

    Fix path to app

--- start_app.sh
+++ start_app.sh
@@ -9,6 +9,7 @@ fi
 MODULE_NAME=${MODULE_NAME:-$DEFAULT_MODULE_NAME}
 VARIABLE_NAME=${VARIABLE_NAME:-app}
 export APP_MODULE=${APP_MODULE:-"$MODULE_NAME:$VARIABLE_NAME"}
+export APP_MODULE=app.main:app
 
 if [ -f /app/gunicorn_conf.py ]; then
     DEFAULT_GUNICORN_CONF=/app/gunicorn_conf.py
