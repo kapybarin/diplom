4b78307e0ec2af2f5b15916c70996df883363bc4
Dmitry Konyshev <dk@noirdjinn.dev>
2020-05-09 16:35:08 +0300

    Refactor start_app.sh

--- start_app.sh
+++ start_app.sh
@@ -1,24 +1,8 @@
 #! /usr/bin/env sh
 set -e
 
-if [ -f /app/app/main.py ]; then
-    DEFAULT_MODULE_NAME=app.main
-elif [ -f /app/main.py ]; then
-    DEFAULT_MODULE_NAME=main
-fi
-MODULE_NAME=${MODULE_NAME:-$DEFAULT_MODULE_NAME}
-VARIABLE_NAME=${VARIABLE_NAME:-app}
-export APP_MODULE=${APP_MODULE:-"$MODULE_NAME:$VARIABLE_NAME"}
 export APP_MODULE=app.main:app
-
-if [ -f /app/gunicorn_conf.py ]; then
-    DEFAULT_GUNICORN_CONF=/app/gunicorn_conf.py
-elif [ -f /app/app/gunicorn_conf.py ]; then
-    DEFAULT_GUNICORN_CONF=/app/app/gunicorn_conf.py
-else
-    DEFAULT_GUNICORN_CONF=/gunicorn_conf.py
-fi
-export GUNICORN_CONF=${GUNICORN_CONF:-$DEFAULT_GUNICORN_CONF}
+export GUNICORN_CONF=/gunicorn_conf.py
 export WORKER_CLASS=${WORKER_CLASS:-"uvicorn.workers.UvicornWorker"}
 
 # If there's a prestart.sh script in the /app directory or other path specified, run it before starting
