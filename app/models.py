f0d9fb6de925f64e1fc8a36283b479a74ca39361
Dmitry Konyshev <dk@noirdjinn.dev>
2020-05-12 22:09:26 +0300

    last 8.0

--- app/models.py
+++ app/models.py
@@ -64,9 +64,12 @@ class Token(db.Entity):
 @orm.db_session
 def setup_data():
     names = ["Ноутбук", "Документы", "Мышь", "Клавиатура", "Маркеры"]
-    for name in names:
-        c = Cell_Type(name=name)
-        db.commit()
+    try:
+        for name in names:
+            c = Cell_Type(name=name)
+            db.commit()
+    except:
+        pass
 
 
 def setup_database():
