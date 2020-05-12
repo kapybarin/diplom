dc7df8ef18ddec323641ffcca1b3de0df727c5e7
Dmitry Konyshev <dk@noirdjinn.dev>
2020-05-12 21:45:16 +0300

    last 2.0

--- setup_data.py
+++ setup_data.py
@@ -1,9 +1,17 @@
 from app.models import setup_database, Cell_Type
 from dotenv import load_dotenv
+from pony.orm import db_session
 
-load_dotenv()
-db = setup_database(True)
-names = ["Ноутбук", "Документы", "Мышь", "Клавиатура", "Маркеры"]
-for name in names:
-    c = Cell_Type(name=name)
-    db.commit()
+
+@db_session
+def main():
+    load_dotenv()
+    db = setup_database(True)
+    names = ["Ноутбук", "Документы", "Мышь", "Клавиатура", "Маркеры"]
+    for name in names:
+        c = Cell_Type(name=name)
+        db.commit()
+
+
+if __name__ == "__main__":
+    main()
