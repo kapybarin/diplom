919050cff5586f595223fd27841e93636e5e0507
Dmitry Konyshev <dk@noirdjinn.dev>
2020-05-12 21:41:44 +0300

    Last fix, I hope

--- setup_data.py
+++ setup_data.py
@@ -1,6 +1,7 @@
 from app.models import setup_database, Cell_Type
+from dotenv import load_dotenv
 
-
+load_dotenv()
 db = setup_database(True)
 names = ["Ноутбук", "Документы", "Мышь", "Клавиатура", "Маркеры"]
 for name in names:
