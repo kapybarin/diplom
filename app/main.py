2e06d4a57650a008bfabbd6a5e23fcc7ff96ba57
Dmitry Konyshev <dk@noirdjinn.dev>
2020-05-21 10:17:12 +0300

    Updated version

--- app/main.py
+++ app/main.py
@@ -30,7 +30,7 @@ def custom_openapi():
         return app.openapi_schema
     openapi_schema = get_openapi(
         title="TakeAndGo",
-        version="0.1.7",
+        version="0.1.8",
         description="Smart bookshelf project",
         routes=app.routes,
     )
